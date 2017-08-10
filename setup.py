638cd245169d54394ba99c6fb1779cb3048908b8
Steven R. Young <stevenryoung@gmail.com>
2017-08-09 20:13:17 -0400

    Moving some files around.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='radiodelay',
+      version='0.1.0',
+      description='Tool for delaying radio to sync to television broadcast.',
+      url='https://github.com/stevenryoung/RadioDelay',
+      author='Steven R. Young',
+      license='GPLv3',
+      packages=['radiodelay'],
+      zip_safe=False)
