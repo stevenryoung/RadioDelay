17dbcc78dbec1d8479c772e685a03c984d1f26cf
Steven R. Young <stevenryoung@gmail.com>
2017-08-09 20:44:40 -0400

    Adding reqs.

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 from setuptools import setup
 
+reqs = ['pyaudio>=0.2.9', 'python-gflags>=2.0.0']
+
 setup(name='radiodelay',
       version='0.1.0',
       description='Tool for delaying radio to sync to television broadcast.',
@@ -7,4 +9,5 @@ setup(name='radiodelay',
       author='Steven R. Young',
       license='GPLv3',
       packages=['radiodelay'],
-      zip_safe=False)
+      zip_safe=False,
+      install_requires=reqs)
