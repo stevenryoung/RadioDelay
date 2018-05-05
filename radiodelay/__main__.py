bd3bf99d0aa80bd537c6dc19fa8ef86878f3d892
Steven R. Young <stevenryoung@gmail.com>
2018-05-05 19:32:43 -0400

    Replaced gflags with argparse and misc cleanup.

--- radiodelay/__main__.py
+++ radiodelay/__main__.py
@@ -19,24 +19,22 @@ from __future__ import print_function
 if hasattr(__builtins__, 'raw_input'):
     input = raw_input
 
-import gflags
-from gflags import FLAGS
 import logging
 import logging.config
 from multiprocessing import Process, Pipe
 import os
 from pkg_resources import Requirement, resource_filename
 import pyaudio
-import sys
+import argparse
+
 
 # Initialize Logging
-filename = resource_filename('radiodelay','radio_delay_log_settings.ini') 
-print(filename)
-logging.config.fileConfig(filename)
+log_settings_file = resource_filename('radiodelay','radio_delay_log_settings.ini') 
+logging.config.fileConfig(log_settings_file)
 LOG = logging.getLogger('radio_delay')
 
 # Some Global Variables
-RD_VERSION = '0.1.0'
+RD_VERSION = '0.2.0'
 DELAY_PROMPT = 'Enter your desired delay in seconds. Enter -1 to quit.\n'
 COPYRIGHT = ('Sports Radio Delay\n'
              'Copyright (C) 2014-2015  Steven Young <stevenryoung@gmail.com>\n'
@@ -44,14 +42,23 @@ COPYRIGHT = ('Sports Radio Delay\n'
              'This is free software, and you are welcome to redistribute it\n'
              'under certain conditions; type "show details" for more info\n')
 
-# Configurable through command line options
-gflags.DEFINE_float('delay', 5.0, 'delay (s)')
-gflags.DEFINE_integer('sample_rate', 44100, 'sample rate (hz)')
-gflags.DEFINE_integer('chunk', 2048, 'chunk size (bytes)')
-gflags.DEFINE_integer('width', 2, 'width')
-gflags.DEFINE_integer('channels', 2, 'number of channels')
-gflags.DEFINE_integer('bffsz', 300, 'size of ring buffer (seconds)')
-gflags.DEFINE_integer('primelen', 5, 'number of chunks to prime output')
+# Parse Arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('--delay', type=float, help='delay (seconds)', 
+                    action='store', default=5.0)
+parser.add_argument('--sample_rate', type=int, help='sample rate (hz)',
+                    action='store', default=44100)
+parser.add_argument('--chunk', type=int, help='chunk size (bytes)',
+                    action='store', default=2048)
+parser.add_argument('--width', type=int, help='width',
+                    action='store', default=2)
+parser.add_argument('--channels', type=int, help='number of channels',
+                    action='store', default=2)
+parser.add_argument('--bffsz', type=int, help='size of ring buffer (seconds)',
+                    action='store', default=300)
+parser.add_argument('--primelen', type=int, help='number of chunks to prime output',
+                    action='store', default=5)
+ARGS = parser.parse_args()
 
 
 def write_terminal(desired_delay):
@@ -60,25 +67,25 @@ def write_terminal(desired_delay):
     print("Delay (seconds): {}".format(desired_delay))
     print(DELAY_PROMPT)
     
-def initialize_stream(audio, flags):
-    return audio.open(format=audio.get_format_from_width(flags.width),
-                    channels=flags.channels,
-                    rate=flags.sample_rate,
+def initialize_stream(audio):
+    return audio.open(format=audio.get_format_from_width(ARGS.width),
+                    channels=ARGS.channels,
+                    rate=ARGS.sample_rate,
                     input=True,
                     output=True,
-                    frames_per_buffer=flags.chunk)
+                    frames_per_buffer=ARGS.chunk)
 
 def delay_loop(conn):
     # Initialize PyAudio
     p = pyaudio.PyAudio()
 
     # Initialize Stream
-    stream = initialize_stream(p, FLAGS)
+    stream = initialize_stream(p)
 
     # Establish some parameters
-    bps = float(FLAGS.sample_rate) / float(FLAGS.chunk)  # blocks per second
-    desireddelay = FLAGS.delay  # delay in seconds
-    buffersecs = FLAGS.bffsz  # size of buffer in seconds
+    bps = float(ARGS.sample_rate) / float(ARGS.chunk)  # blocks per second
+    desireddelay = ARGS.delay  # delay in seconds
+    buffersecs = ARGS.bffsz  # size of buffer in seconds
 
     # Create buffer
     bfflen = int(buffersecs * bps)
@@ -89,7 +96,7 @@ def delay_loop(conn):
     ridx = 0  # pointer to read position
 
     # Prewrite empty data to buffer to be read
-    blocksize = len(stream.read(FLAGS.chunk))
+    blocksize = len(stream.read(ARGS.chunk))
     for tmp in range(bfflen):
         buff[tmp] = '0' * blocksize
 
@@ -97,24 +104,24 @@ def delay_loop(conn):
     write_terminal(desireddelay)
 
     # Preload data into output to avoid stuttering during playback
-    for tmp in range(FLAGS.primelen):
-        stream.write('0' * blocksize, FLAGS.chunk)
+    for tmp in range(ARGS.primelen):
+        stream.write('0' * blocksize, ARGS.chunk)
 
     # Loop until program terminates
     while True:
         # Read next input
-        buff[widx] = stream.read(FLAGS.chunk)
+        buff[widx] = stream.read(ARGS.chunk)
 
         # Write output
         try:
-            stream.write(buff[ridx], FLAGS.chunk, exception_on_underflow=True)
+            stream.write(buff[ridx], ARGS.chunk, exception_on_underflow=True)
         except IOError:  # underflow, priming the output
-            LOG.warning("Underflow occurred", exc_info=True)
+            LOG.warning('Underflow occurred', exc_info=True)
             stream.stop_stream()
             stream.close()
-            stream = initialize_stream(p, FLAGS)
-            for i in range(FLAGS.primelen):
-                stream.write('0' * blocksize, FLAGS.chunk, exception_on_underflow=False)
+            stream = initialize_stream(p)
+            for i in range(ARGS.primelen):
+                stream.write('0' * blocksize, ARGS.chunk, exception_on_underflow=False)
 
         # Update write and read pointers
         widx += 1
@@ -136,16 +143,9 @@ def delay_loop(conn):
                 break
 
 
-def main(argv):
+def main():
     # Print some info to log
-    LOG.info("Radio Delay - {}".format(RD_VERSION))
-
-    # Read flags
-    try:
-      argv = FLAGS(argv)  # parse flags
-    except gflags.FlagsError as e:
-      print('%s\\nUsage: %s ARGS\\n%s' % (e, sys.argv[0], FLAGS))
-      sys.exit(1)
+    LOG.info('Radio Delay - {}'.format(RD_VERSION))
 
     # Establish pipe for delay process
     pconn1, cconn1 = Pipe()
@@ -163,15 +163,15 @@ def main(argv):
             elif inp > 0.0:  # Update delay
                 pconn1.send(inp)
             else:
-                print("Please use a delay longer than 0 sec.")
+                print('Please use a delay longer than 0 sec.')
         except:
-            if "show" in inp:  # Give link to license
-                print("See the copy of GPLv3 provided with this program")
-                print("or <http://www.gnu.org/licenses/> for more details.")
+            if 'show' in inp:  # Give link to license
+                print('See the copy of GPLv3 provided with this program')
+                print('or <http://www.gnu.org/licenses/> for more details.')
             else:
-                print("Improper input.")
+                print('Improper input.')
     p1.join()
 
 
 if __name__ == '__main__':
-    main(sys.argv)
+    main()
