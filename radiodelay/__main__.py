88b495238a323630895e94c6673b622e1e4d997a
Steven R. Young <stevenryoung@gmail.com>
2021-04-07 21:21:37 -0400

    Black formatting.

--- radiodelay/__main__.py
+++ radiodelay/__main__.py
@@ -16,7 +16,8 @@
 
 # Python2/3 Compatibility
 from __future__ import print_function
-if hasattr(__builtins__, 'raw_input'):
+
+if hasattr(__builtins__, "raw_input"):
     input = raw_input
 
 import logging
@@ -29,51 +30,70 @@ import argparse
 
 
 # Initialize Logging
-log_settings_file = resource_filename('radiodelay','radio_delay_log_settings.ini') 
+log_settings_file = resource_filename("radiodelay", "radio_delay_log_settings.ini")
 logging.config.fileConfig(log_settings_file)
-LOG = logging.getLogger('radio_delay')
+LOG = logging.getLogger("radio_delay")
 
 # Some Global Variables
-RD_VERSION = '0.2.0-dev'
-DELAY_PROMPT = 'Enter your desired delay in seconds. Enter -1 to quit.\n'
-COPYRIGHT = ('Sports Radio Delay\n'
-             'Copyright (C) 2014-2015  Steven Young <stevenryoung@gmail.com>\n'
-             'This program comes with ABSOLUTELY NO WARRANTY.\n'
-             'This is free software, and you are welcome to redistribute it\n'
-             'under certain conditions; type "show details" for more info\n')
+RD_VERSION = "0.2.0-dev"
+DELAY_PROMPT = "Enter your desired delay in seconds. Enter -1 to quit.\n"
+COPYRIGHT = (
+    "Sports Radio Delay\n"
+    "Copyright (C) 2014-2015  Steven Young <stevenryoung@gmail.com>\n"
+    "This program comes with ABSOLUTELY NO WARRANTY.\n"
+    "This is free software, and you are welcome to redistribute it\n"
+    'under certain conditions; type "show details" for more info\n'
+)
 
 # Parse Arguments
 parser = argparse.ArgumentParser()
-parser.add_argument('--delay', type=float, help='delay (seconds)', 
-                    action='store', default=5.0)
-parser.add_argument('--sample_rate', type=int, help='sample rate (hz)',
-                    action='store', default=44100)
-parser.add_argument('--chunk', type=int, help='chunk size (bytes)',
-                    action='store', default=2048)
-parser.add_argument('--width', type=int, help='width',
-                    action='store', default=2)
-parser.add_argument('--channels', type=int, help='number of channels',
-                    action='store', default=2)
-parser.add_argument('--bffsz', type=int, help='size of ring buffer (seconds)',
-                    action='store', default=300)
-parser.add_argument('--primelen', type=int, help='number of chunks to prime output',
-                    action='store', default=5)
+parser.add_argument(
+    "--delay", type=float, help="delay (seconds)", action="store", default=5.0
+)
+parser.add_argument(
+    "--sample_rate", type=int, help="sample rate (hz)", action="store", default=44100
+)
+parser.add_argument(
+    "--chunk", type=int, help="chunk size (bytes)", action="store", default=2048
+)
+parser.add_argument("--width", type=int, help="width", action="store", default=2)
+parser.add_argument(
+    "--channels", type=int, help="number of channels", action="store", default=2
+)
+parser.add_argument(
+    "--bffsz",
+    type=int,
+    help="size of ring buffer (seconds)",
+    action="store",
+    default=300,
+)
+parser.add_argument(
+    "--primelen",
+    type=int,
+    help="number of chunks to prime output",
+    action="store",
+    default=5,
+)
 ARGS = parser.parse_args()
 
 
 def write_terminal(desired_delay):
-    os.system('cls' if os.name == 'nt' else 'clear')
+    os.system("cls" if os.name == "nt" else "clear")
     print(COPYRIGHT)
     print("Delay (seconds): {}".format(desired_delay))
     print(DELAY_PROMPT)
-    
+
+
 def initialize_stream(audio):
-    return audio.open(format=audio.get_format_from_width(ARGS.width),
-                    channels=ARGS.channels,
-                    rate=ARGS.sample_rate,
-                    input=True,
-                    output=True,
-                    frames_per_buffer=ARGS.chunk)
+    return audio.open(
+        format=audio.get_format_from_width(ARGS.width),
+        channels=ARGS.channels,
+        rate=ARGS.sample_rate,
+        input=True,
+        output=True,
+        frames_per_buffer=ARGS.chunk,
+    )
+
 
 def delay_loop(conn):
     # Initialize PyAudio
@@ -98,14 +118,14 @@ def delay_loop(conn):
     # Prewrite empty data to buffer to be read
     blocksize = len(stream.read(ARGS.chunk))
     for tmp in range(bfflen):
-        buff[tmp] = '0' * blocksize
+        buff[tmp] = "0" * blocksize
 
     # Write to command prompt
     write_terminal(desireddelay)
 
     # Preload data into output to avoid stuttering during playback
     for tmp in range(ARGS.primelen):
-        stream.write('0' * blocksize, ARGS.chunk)
+        stream.write("0" * blocksize, ARGS.chunk)
 
     # Loop until program terminates
     while True:
@@ -116,12 +136,12 @@ def delay_loop(conn):
         try:
             stream.write(buff[ridx], ARGS.chunk, exception_on_underflow=True)
         except IOError:  # underflow, priming the output
-            LOG.warning('Underflow occurred', exc_info=True)
+            LOG.warning("Underflow occurred", exc_info=True)
             stream.stop_stream()
             stream.close()
             stream = initialize_stream(p)
             for i in range(ARGS.primelen):
-                stream.write('0' * blocksize, ARGS.chunk, exception_on_underflow=False)
+                stream.write("0" * blocksize, ARGS.chunk, exception_on_underflow=False)
 
         # Update write and read pointers
         widx += 1
@@ -145,7 +165,7 @@ def delay_loop(conn):
 
 def main():
     # Print some info to log
-    LOG.info('Radio Delay - {}'.format(RD_VERSION))
+    LOG.info("Radio Delay - {}".format(RD_VERSION))
 
     # Establish pipe for delay process
     pconn1, cconn1 = Pipe()
@@ -163,15 +183,15 @@ def main():
             elif inp > 0.0:  # Update delay
                 pconn1.send(inp)
             else:
-                print('Please use a delay longer than 0 sec.')
+                print("Please use a delay longer than 0 sec.")
         except:
-            if 'show' in inp:  # Give link to license
-                print('See the copy of GPLv3 provided with this program')
-                print('or <http://www.gnu.org/licenses/> for more details.')
+            if "show" in inp:  # Give link to license
+                print("See the copy of GPLv3 provided with this program")
+                print("or <http://www.gnu.org/licenses/> for more details.")
             else:
-                print('Improper input.')
+                print("Improper input.")
     p1.join()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- setup.py
+++ setup.py
@@ -1,14 +1,16 @@
 from setuptools import setup
 
-reqs = ['pyaudio>=0.2.9']
+reqs = ["pyaudio>=0.2.9"]
 
-setup(name='radiodelay',
-      version='0.2.0-dev',
-      description='Tool for delaying radio to sync to television broadcast.',
-      url='https://github.com/stevenryoung/RadioDelay',
-      author='Steven R. Young',
-      license='GPLv3',
-      packages=['radiodelay'],
-      zip_safe=False,
-      install_requires=reqs,
-      package_data={'radiodelay': ['*.ini']})
+setup(
+    name="radiodelay",
+    version="0.2.0-dev",
+    description="Tool for delaying radio to sync to television broadcast.",
+    url="https://github.com/stevenryoung/RadioDelay",
+    author="Steven R. Young",
+    license="GPLv3",
+    packages=["radiodelay"],
+    zip_safe=False,
+    install_requires=reqs,
+    package_data={"radiodelay": ["*.ini"]},
+)
